5a5f6e127d86a02b476c0b0f82d845a5f7fa66b1
takuya <1911140027r@ed.fuk.kindai.ac.jp>
2019-10-15 18:24:17 +0900

    購入画面のプロジェクトの作成

---
+++ app/src/main/java/com/example/kakicomeets_buy/Kakicomeets_Buy_Activity.java
@@ -0,0 +1,14 @@
+package com.example.kakicomeets_buy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kakicomeets_Buy_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kakicomeets_buy);
+    }
+}
